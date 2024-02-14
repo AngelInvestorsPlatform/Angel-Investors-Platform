22eb8233d2ee5301311018ca6317de80973ee01d
ASEELALOMARI <392206377@qu.edu.sa>
2024-02-15 01:55:50 +0300

    add auth files

---
+++ Angel_Investors_Project/project_backend/users/urls.py
@@ -0,0 +1,11 @@
+# users/urls.py
+from django.urls import path
+
+#from .views import RegisterUser, LoginUser, LogoutUser
+
+urlpatterns = [
+    #path('register/', RegisterUser.as_view(), name='register'),
+    #select
+    #path('login/', LoginUser.as_view(), name='login'),
+    #path('logout/', LogoutUser.as_view(), name='logout'),
+]
