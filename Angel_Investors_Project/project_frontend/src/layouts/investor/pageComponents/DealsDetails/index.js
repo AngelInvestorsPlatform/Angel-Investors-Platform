88f6eff73900501b23f55d561adcd1e04f004d04
ASEELALOMARI <392206377@qu.edu.sa>
2024-03-28 07:56:18 +0300

    (raghad code) DealsDetails (edited, Not ready)

--- Angel_Investors_Project/project_frontend/src/layouts/investor/pageComponents/DealsDetails/index.js
+++ Angel_Investors_Project/project_frontend/src/layouts/investor/pageComponents/DealsDetails/index.js
@@ -19,19 +19,18 @@ import DealsInfoCard from "examples/Cards/InfoCards/DealsInfoCard";
 import typography from "assets/theme/base/typography";
 
 // Investor layout components
-import InvestorNavbar from 'layouts/investor/components/InvestorNavbar';
+import InvestorNavbar from "layouts/investor/components/InvestorNavbar";
 import Projects from "layouts/investor/components/Projects";
 
 // images
-import logoInvesion from "assets/images/small-logos/logo-invision.svg";
-
+import logoInvesion from "assets/images/small-logos/logoinvision.svg";
 
 function DealsDetails() {
-    const name = "Aseel";
-    const job = "CEO";
+  const name = "Ahmad Nasser";
+  const job = "CEO";
 
-    // User information
-const userInfo = {
+  // User information
+  const userInfo = {
     fullName: "Alec M. Thompson",
     email: "alecthompson@mail.com",
     mobile: "(44) 123 1234 123",
@@ -39,68 +38,70 @@ const userInfo = {
     experience: "1-2 years",
   };
   // Description
-const descriptionInfo = "Hi, I’m Alec Thompson, Decisions: If you can’t decide, the answer is no. If two equally difficult paths, choose the one more painful in the short term (pain avoidance is creating an illusion of equality).";
+  const descriptionInfo = "Hi, I’m Alec Thompson, Decisions: If you can’t decide, the answer is no. If two equally difficult paths, choose the one more painful in the short term (pain avoidance is creating an illusion of equality).";
 
-// Sectors
-const sectorsInfo = ["Biotech", "Adtech", "Analytics", "Market"];
+  // Sectors
+  const sectorsInfo = ["Biotech", "Adtech", "Analytics", "Market"];
 
   return (
     <PageLayout px={10}>
-        <SoftBox px={10}>
-      <DashboardNavbar />
-        <SoftBox py={3}>  
-        <SoftBox py={10}>  
-        {/* Startup Name Card */}
-        <Card
-        sx={{
-          backdropFilter:`saturate(200%) blur(30px)`,
-          backgroundColor: ({ functions: { rgba }, palette: { white } }) => rgba(white.main, 0.8),
-          boxShadow: ({ boxShadows: { navbarBoxShadow } }) => navbarBoxShadow,
-          position: "relative",
-          mt: -8,
-          mx: 3,
-          py: 2,
-          px: 2,
-        }}>
-        <Grid container spacing={3} alignItems="center">
-          <Grid item>
-            <SoftAvatar
-              src={logoInvesion}
-              alt="profile-image"
-              variant="rounded"
-              size="xl"
-              shadow="sm"
-            />
-          </Grid>
-          <Grid item>
-            <SoftBox height="100%" mt={0.5} lineHeight={1}>
-              <SoftTypography variant="h5" fontWeight="medium">
-               {name}
-              </SoftTypography>
-              <SoftTypography variant="button" color="text" fontWeight="medium">
-               {job}
-              </SoftTypography>
+      <SoftBox px={10}>
+        <DashboardNavbar />
+        <SoftBox py={3}>
+          <SoftBox py={10}>
+            {/* Startup Name Card */}
+            <Card
+              sx={{
+                backdropFilter: `saturate(200%) blur(30px)`,
+                backgroundColor: ({ functions: { rgba }, palette: { white } }) =>
+                  rgba(white.main, 0.8),
+                boxShadow: ({ boxShadows: { navbarBoxShadow } }) => navbarBoxShadow,
+                position: "relative",
+                mt: -8,
+                mx: 3,
+                py: 2,
+                px: 2,
+              }}
+            >
+              <Grid container spacing={3} alignItems="center">
+                <Grid item>
+                  <SoftAvatar
+                    src={logoInvesion}
+                    alt="profile-image"
+                    variant="rounded"
+                    size="xl"
+                    shadow="sm"
+                  />
+                </Grid>
+                <Grid item>
+                  <SoftBox height="100%" mt={0.5} lineHeight={1}>
+                    <SoftTypography variant="h5" fontWeight="medium">
+                      {name}
+                    </SoftTypography>
+                    <SoftTypography variant="button" color="text" fontWeight="medium">
+                      {job}
+                    </SoftTypography>
+                  </SoftBox>
+                </Grid>
+              </Grid>
+            </Card>
+            {/* Deals Info Card */}
+            <SoftBox px={6} mt={5} mb={3}>
+              <Grid container spacing={3}>
+                <Grid item xs={12} md={6} xl={6}>
+                  <DealsInfoCard
+                    title="Invest"
+                    description={descriptionInfo}
+                    info={userInfo}
+                    sectors={sectorsInfo}
+                    action={{ route: "", tooltip: "Edit Profile" }}
+                  />
+                </Grid>
+              </Grid>
             </SoftBox>
-          </Grid>
-        </Grid>
-      </Card> 
-        {/* Deals Info Card */}
-        <SoftBox px={6} mt={5} mb={3}>
-        <Grid container spacing={3}>
-          <Grid item xs={12} md={6} xl={6}>
-          <DealsInfoCard
-              title="About Investor"
-              description={descriptionInfo}
-              info={userInfo}
-              sectors={sectorsInfo}
-              action={{ route: "", tooltip: "Edit Profile" }}
-            />
-          </Grid>
-        </Grid>
-      </SoftBox>
+          </SoftBox>
         </SoftBox>
-        </SoftBox>
-      <Footer />
+        <Footer />
       </SoftBox>
     </PageLayout>
   );
