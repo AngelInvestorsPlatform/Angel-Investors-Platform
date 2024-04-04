3152d4f91f81d76cf81e39136b3808cec66485b2
ASEELALOMARI <392206377@qu.edu.sa>
2024-04-04 09:34:41 +0300

    edit Active deals

--- Angel_Investors_Project/project_frontend/src/layouts/LeadInvestor/pageComponents/ActiveDeals/index.js
+++ Angel_Investors_Project/project_frontend/src/layouts/LeadInvestor/pageComponents/ActiveDeals/index.js
@@ -21,20 +21,61 @@ import LeadNavbar from 'layouts/LeadInvestor/components/LeadNavbar';
 import Projects from "layouts/investor/components/Projects";
 
 // Data
+import ActiveDealsData from "layouts/LeadInvestor/pageComponents/ActiveDeals/data/ActiveDealsData";
 
 
 function YourDeals() {
 
+  const { columns, rows } = ActiveDealsData;
 
   return (
     <DashboardLayout>
       <DashboardNavbar />
       <LeadNavbar />
       <SoftBox py={3}>
-        {/*  your Deals table start here */}
-        <Projects/>
+        {/*Deals table start here */}
+        <SoftBox py={3}>
+        <SoftBox mb={3}>
+          <Card>
+            <SoftBox display="flex" justifyContent="space-between" alignItems="center" p={3}>
+              <SoftBox>
+                <SoftTypography variant="h4" color="info" fontWeight="regular" textGradient>
+                  {" "}
+                  Active Deals
+                </SoftTypography>
+                <SoftBox display="flex" alignItems="center" lineHeight={0}>
+                  <Icon
+                    sx={{
+                      fontWeight: "bold",
+                      color: ({ palette: { info } }) => info.main,
+                      mt: -0.3,
+                    }}
+                  >
+                    equalizer
+                  </Icon>
+                  <SoftTypography variant="button" fontWeight="regular" color="text">
+                    &nbsp; Browse all the <strong> Active Deals</strong> and invest right away!
+                  </SoftTypography>
+                </SoftBox>
+              </SoftBox>
+            </SoftBox>
+            <SoftBox
+              sx={{
+                "& .MuiTableRow-root:not(:last-child)": {
+                  "& td": {
+                    borderBottom: ({ borders: { borderWidth, borderColor } }) =>
+                      `${borderWidth[1]} solid ${borderColor}`,
+                  },
+                },
+              }}
+            >
+              <Table columns={columns} rows={rows} />
+            </SoftBox>
+          </Card>
+        </SoftBox>
+      </SoftBox>
 
-        {/*  your Deals table end here */}
+        {/* Deals table end here */}
       </SoftBox>
       <Footer />
     </DashboardLayout>
